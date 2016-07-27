ad2fe429be31d1785f7de3a226a935884dda3c47
Alexander <xander.loh@gmail.com>
2016-07-27 12:27:07 +0800

    added facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 	root 'users#homepage'
+	 get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+	 resources :users, only: [:show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
